2c9123d192ee38429c4e0ea11274660a4f749083
danielblokus <daniel.blokus@gmail.com>
2019-10-04 17:52:12 +0200

    Add example test with schema validation

--- src/test/java/io/github/danielblokus/SchemaValidatorTest.java
+++ src/test/java/io/github/danielblokus/SchemaValidatorTest.java
@@ -3,11 +3,15 @@ package io.github.danielblokus;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.junit.Assert.fail;
 
 public class SchemaValidatorTest {
 
     private final static String ANYTHING_URL = "https://httpbin.org/anything";
+    private final static String UUID_URL = "https://httpbin.org/uuid";
+
+    private final static String UUID_SCHEMA_FILE = "response.json";
 
     @Test
     public void firstTest() {
@@ -22,5 +26,15 @@ public class SchemaValidatorTest {
         .then()
             .assertThat()
                 .statusCode(200);
+    }
+
+    @Test
+    public void givenUrl_whenJsonResponseConformsToSchema_thenCorrect() {
+        given()
+        .when()
+            .get(UUID_URL)
+        .then()
+            .assertThat()
+                .body(matchesJsonSchemaInClasspath(UUID_SCHEMA_FILE));
     }
 }
