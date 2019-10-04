2f5b62f5cc728e2ea0a9795f35bc59b6718c0526
danielblokus <daniel.blokus@gmail.com>
2019-10-04 17:38:51 +0200

    Add example test with RestAssured

--- src/test/java/io/github/danielblokus/SchemaValidatorTest.java
+++ src/test/java/io/github/danielblokus/SchemaValidatorTest.java
@@ -2,12 +2,25 @@ package io.github.danielblokus;
 
 import org.junit.Test;
 
+import static io.restassured.RestAssured.given;
 import static org.junit.Assert.fail;
 
 public class SchemaValidatorTest {
 
+    private final static String ANYTHING_URL = "https://httpbin.org/anything";
+
     @Test
     public void firstTest() {
         fail("FAIL");
+    }
+
+    @Test
+    public void secondTest() {
+        given()
+        .when()
+            .get(ANYTHING_URL)
+        .then()
+            .assertThat()
+                .statusCode(200);
     }
 }
